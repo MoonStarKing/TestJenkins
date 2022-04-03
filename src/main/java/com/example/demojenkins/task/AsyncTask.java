2849b104da80585b2dfbaa7c2f099a3371e20a49
DESKTOP-JVKTI5U\Moon <389527082@qq.com>
2022-04-03 11:18:04 +0800

    我今天要做实验

--- src/main/java/com/example/demojenkins/task/AsyncTask.java
+++ src/main/java/com/example/demojenkins/task/AsyncTask.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Async
 public class AsyncTask {
+    public static void main(String[] args) {
+        System.out.println(123333);
+    }
 }
